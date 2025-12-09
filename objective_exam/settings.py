e2dee7cfcbafc6999c6ab3afc21892abc16e2ecc
binay <tbinay5@gmail.com>
2025-12-09 15:00:29 +0545

    setting update

--- objective_exam/settings.py
+++ objective_exam/settings.py
@@ -10,8 +10,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'django-insecure-change-me-please')
 DEBUG = os.environ.get('DJANGO_DEBUG', 'True') == 'True'
 
-ALLOWED_HOSTS = os.environ.get('DJANGO_ALLOWED_HOSTS', 'localhost,127.0.0.1')
-ALLOWED_HOSTS = [h.strip() for h in ALLOWED_HOSTS.split(',') if h.strip()]
+#ALLOWED_HOSTS = os.environ.get('DJANGO_ALLOWED_HOSTS', 'localhost,127.0.0.1')
+#ALLOWED_HOSTS = [h.strip() for h in ALLOWED_HOSTS.split(',') if h.strip()]
+
+DEBUG = False
+
+ALLOWED_HOSTS = ['nptor.com', 'www.nptor.com']
 
 # ============================================================
 # Application definition
--- objective_exam/wsgi.py
+++ objective_exam/wsgi.py
@@ -1,4 +1,8 @@
+# objective_exam/wsgi.py
+
 import os
 from django.core.wsgi import get_wsgi_application
-os.environ.setdefault('DJANGO_SETTINGS_MODULE','objective_exam.settings')
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "objective_exam.settings")
+
 application = get_wsgi_application()
