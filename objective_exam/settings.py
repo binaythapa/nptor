f2b83dcde7728bc87e760aa1084f543d96ec7bc5
binay <tbinay5@gmail.com>
2025-12-18 22:50:40 +0545

    update Debuf to True

--- objective_exam/settings.py
+++ objective_exam/settings.py
@@ -16,9 +16,9 @@ SECRET_KEY = os.environ.get(
     "django-insecure-change-me-please"
 )
 
-
-DEBUG = os.environ.get("DJANGO_DEBUG", "True").lower() == "true"
 '''
+DEBUG = os.environ.get("DJANGO_DEBUG", "True").lower() == "true"
+
 ALLOWED_HOSTS = (
     ["localhost", "127.0.0.1"]
     if DEBUG
