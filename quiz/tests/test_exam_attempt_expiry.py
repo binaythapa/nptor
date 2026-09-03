4c45c2ae09cadaa8dc31a64023775ebd1c6667b8
Binay Thapa <37831189+binaythapa@users.noreply.github.com>
2026-09-03 17:30:49 +0545

    Refine expiry regression test for timer submit target

--- quiz/tests/test_exam_attempt_expiry.py
+++ quiz/tests/test_exam_attempt_expiry.py
@@ -1,9 +1,6 @@
-from datetime import timedelta
-
 from django.contrib.auth import get_user_model
 from django.test import TestCase
 from django.urls import reverse
-from django.utils import timezone
 
 from quiz.models import Choice, Exam, Question, UserAnswer, UserExam
 
@@ -12,12 +9,12 @@ User = get_user_model()
 
 
 class ExamAttemptExpiryTests(TestCase):
-    def test_expired_post_saves_current_answer_before_submission(self):
-        user = User.objects.create_user(
+    def setUp(self):
+        self.user = User.objects.create_user(
             username="expiry-test-user",
             password="test-password",
         )
-        exam = Exam.objects.create(
+        self.exam = Exam.objects.create(
             title="Expiry Test Exam",
             question_count=1,
             duration_seconds=60,
@@ -25,60 +22,42 @@ class ExamAttemptExpiryTests(TestCase):
             is_free=True,
             is_published=True,
         )
-        question = Question.objects.create(
+        self.question = Question.objects.create(
             text="Which option is correct?",
             question_type=Question.SINGLE,
             difficulty=Question.EASY,
         )
-        correct = Choice.objects.create(
-            question=question,
+        self.correct = Choice.objects.create(
+            question=self.question,
             text="Correct",
             is_correct=True,
         )
-        ue = UserExam.objects.create(
-            user=user,
-            exam=exam,
-            question_order=[question.id],
+        self.ue = UserExam.objects.create(
+            user=self.user,
+            exam=self.exam,
+            question_order=[self.question.id],
             current_index=0,
         )
         UserAnswer.objects.create(
-            user_exam=ue,
-            question=question,
+            user_exam=self.ue,
+            question=self.question,
         )
-        UserExam.objects.filter(pk=ue.pk).update(
-            started_at=timezone.now() - timedelta(seconds=120),
-        )
+        self.client.force_login(self.user)
 
-        self.client.force_login(user)
-
-        response = self.client.post(
+    def test_timer_expiry_submits_directly_to_exam_submit_endpoint(self):
+        response = self.client.get(
             reverse(
                 "quiz:exam_question",
-                args=[ue.id, 0],
-            ),
-            {
-                f"question_{question.id}": str(correct.id),
-                "nav": "review",
-            },
+                args=[self.ue.id, 0],
+            )
         )
 
-        self.assertEqual(
-            response.status_code,
-            302,
-        )
-        self.assertEqual(
-            response.url,
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(
+            response,
             reverse(
                 "quiz:exam_submit",
-                args=[ue.id],
-            ),
+                args=[self.ue.id],
+            )
         )
-
-        answer = UserAnswer.objects.get(
-            user_exam=ue,
-            question=question,
-        )
-        self.assertEqual(
-            answer.choice_id,
-            correct.id,
-        )
+        self.assertContains(response, "form.action =")
