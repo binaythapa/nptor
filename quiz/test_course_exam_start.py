90b8b5d080a1be483ad00b3b0cecd95476037963
Binay Thapa <37831189+binaythapa@users.noreply.github.com>
2026-09-05 11:59:14 +0545

    test: cover course practice return redirect

--- quiz/test_course_exam_start.py
+++ quiz/test_course_exam_start.py
@@ -5,6 +5,7 @@ from django.test import SimpleTestCase
 
 from quiz.views.course_exam_start import _course_access_allows_quiz
 from quiz.views.exam_submission import _course_quiz_return_redirect
+from quiz.views.practice import _course_practice_return_redirect
 
 
 class CourseExamAccessTests(SimpleTestCase):
@@ -63,3 +64,28 @@ class CourseExamReturnRedirectTests(SimpleTestCase):
         request = SimpleNamespace(session={})
 
         self.assertIsNone(_course_quiz_return_redirect(request))
+
+
+class CoursePracticeReturnRedirectTests(SimpleTestCase):
+    def test_course_practice_returns_to_course_lesson(self):
+        request = SimpleNamespace(
+            session={
+                "course_practice_context": {
+                    "course_slug": "demo-aws-cloud-course",
+                    "lesson_id": 7,
+                }
+            }
+        )
+
+        response = _course_practice_return_redirect(request)
+
+        self.assertIsNotNone(response)
+        self.assertEqual(
+            response.url,
+            "/courses/demo-aws-cloud-course/learn/7/",
+        )
+
+    def test_standalone_practice_has_no_course_return_redirect(self):
+        request = SimpleNamespace(session={})
+
+        self.assertIsNone(_course_practice_return_redirect(request))
