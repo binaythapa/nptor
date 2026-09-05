d2a8b69fd57991bb8a1793c13ff20f905afc7413
Binay Thapa <37831189+binaythapa@users.noreply.github.com>
2026-09-06 02:01:28 +0545

    feat(cv): add CV app configuration

---
+++ cv/apps.py
@@ -0,0 +1,8 @@
+from django.apps import AppConfig
+
+
+class CVConfig(AppConfig):
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "cv"
+    label = "cv"
+    verbose_name = "CV Builder"
