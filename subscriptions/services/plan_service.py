0cd3f455aaef90355314f8a2c98873d72a9ddf6a
Binay Thapa <37831189+binaythapa@users.noreply.github.com>
2026-09-05 17:19:27 +0545

    Make exam pricing plan-based and independent

--- subscriptions/services/plan_service.py
+++ subscriptions/services/plan_service.py
@@ -1,7 +1,5 @@
 from django.shortcuts import get_object_or_404
 
-from subscriptions.models import SubscriptionPlan
-
 
 def _active_plans(plans):
     return plans.filter(is_active=True).order_by("price", "id")
@@ -22,26 +20,9 @@ def get_plan_for_course(course, plan_id=None):
     return plans.first()
 
 
-def get_default_plan():
-    return (
-        SubscriptionPlan.objects
-        .filter(is_active=True)
-        .order_by("price", "id")
-        .first()
-    )
-
-
 def get_plan_for_exam(exam, plan_id=None):
+    """Return an active plan explicitly attached to the reusable exam."""
+    plans = _active_plans(exam.subscription_plans)
     if plan_id:
-        return get_object_or_404(
-            SubscriptionPlan,
-            id=plan_id,
-            is_active=True,
-        )
-
-    if exam.track_id:
-        plan = get_plan_for_track(exam.track)
-        if plan:
-            return plan
-
-    return get_default_plan()
+        return get_object_or_404(plans, id=plan_id)
+    return plans.first()
