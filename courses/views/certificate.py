b5a1dc19f3994357bb93ac28c402f8eed65fb717
Binay Thapa <37831189+binaythapa@users.noreply.github.com>
2026-09-05 12:46:02 +0545

    feat: add public certificate PDF delivery

--- courses/views/certificate.py
+++ courses/views/certificate.py
@@ -1,12 +1,18 @@
+from django.http import HttpResponse
 from django.shortcuts import get_object_or_404, render
 
 from courses.models import CourseCertificate
+from courses.services.certificate_pdf import generate_certificate_pdf
+
+
+def _certificate_queryset():
+    return CourseCertificate.objects.select_related("user", "course")
 
 
 def certificate_verify(request, certificate_id):
     """Publicly verify an NPTOR course-completion certificate."""
     certificate = get_object_or_404(
-        CourseCertificate.objects.select_related("user", "course"),
+        _certificate_queryset(),
         certificate_id=certificate_id,
     )
 
@@ -24,3 +30,23 @@ def certificate_verify(request, certificate_id):
             "sections": sections,
         },
     )
+
+
+def certificate_download(request, certificate_id):
+    """Download a verified certificate using its public certificate ID."""
+    certificate = get_object_or_404(
+        _certificate_queryset(),
+        certificate_id=certificate_id,
+    )
+
+    pdf = generate_certificate_pdf(
+        certificate.user,
+        certificate.course,
+        certificate,
+    )
+
+    response = HttpResponse(pdf, content_type="application/pdf")
+    response["Content-Disposition"] = (
+        f'attachment; filename="NPTOR-{certificate.certificate_id}.pdf"'
+    )
+    return response
