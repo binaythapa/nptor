c0f419089af0e575369761bf63e5bd73e0f59e2f
Binay Thapa <37831189+binaythapa@users.noreply.github.com>
2026-09-07 13:21:44 +0545

    feat(org): export portal tenant models

--- organizations/models/__init__.py
+++ organizations/models/__init__.py
@@ -3,3 +3,7 @@ from .membership import OrganizationMember
 from .role import OrganizationRole
 from .assignment import ResourceAssignment
 from .access import ResourceAccess
+from .profile import OrganizationProfile
+from .portal import OrganizationPortalConfig
+from .domain import OrganizationDomain
+from .portfolio import OrganizationPortfolioSection
