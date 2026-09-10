a06a60fa159bffc1e342f9fa4465370766554585
Binay Thapa <37831189+binaythapa@users.noreply.github.com>
2026-09-11 01:18:07 +0545

    test: align enrollment uniqueness with service boundary

--- organizations/test_student_academic_models.py
+++ organizations/test_student_academic_models.py
@@ -2,16 +2,9 @@ from django.contrib.auth import get_user_model
 from django.core.exceptions import ValidationError
 from django.test import TestCase
 
-from organizations.models import (
-    AcademicYear,
-    ClassSection,
-    Organization,
-    OrganizationClass,
-    OrganizationMember,
-    OrganizationStudent,
-    StudentEnrollment,
-)
+from organizations.models import AcademicYear, ClassSection, Organization, OrganizationClass, OrganizationMember, OrganizationStudent, StudentEnrollment
 from organizations.models.role import OrganizationRole
+from organizations.services.enrollment import enroll_student
 
 User = get_user_model()
 
@@ -21,8 +14,10 @@ class StudentAcademicModelTests(TestCase):
         self.org_a = Organization.objects.create(name="School A", slug="academic-a", org_type=Organization.TYPE_SCHOOL)
         self.org_b = Organization.objects.create(name="School B", slug="academic-b", org_type=Organization.TYPE_SCHOOL)
         self.user = User.objects.create_user(username="academic-student", password="password")
+        self.admin = User.objects.create_user(username="academic-admin", password="password")
         OrganizationMember.objects.create(user=self.user, organization=self.org_a, role=OrganizationRole.STUDENT)
         OrganizationMember.objects.create(user=self.user, organization=self.org_b, role=OrganizationRole.STUDENT)
+        OrganizationMember.objects.create(user=self.admin, organization=self.org_a, role=OrganizationRole.ORG_ADMIN)
 
     def test_student_identity_is_unique_per_organization(self):
         OrganizationStudent.objects.create(organization=self.org_a, user=self.user)
@@ -35,10 +30,9 @@ class StudentAcademicModelTests(TestCase):
         year = AcademicYear.objects.create(organization=self.org_a, name="2026/27", start_date="2026-04-01", end_date="2027-03-31")
         klass = OrganizationClass.objects.create(organization=self.org_a, name="Grade 10")
         section = ClassSection.objects.create(organization=self.org_a, academic_year=year, class_group=klass, name="A")
-        StudentEnrollment.objects.create(student=student, academic_year=year, class_section=section)
-        second = StudentEnrollment(student=student, academic_year=year, class_section=section)
+        enroll_student(actor=self.admin, student=student, section=section)
         with self.assertRaises(ValidationError):
-            second.full_clean()
+            enroll_student(actor=self.admin, student=student, section=ClassSection.objects.create(organization=self.org_a, academic_year=year, class_group=klass, name="B"))
 
     def test_class_section_and_student_must_share_organization(self):
         student = OrganizationStudent.objects.create(organization=self.org_a, user=self.user)
