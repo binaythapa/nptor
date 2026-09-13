328886c0a341c7e87a87cc0939ff3ee48ff814fe
Binay Thapa <37831189+binaythapa@users.noreply.github.com>
2026-09-14 00:32:12 +0545

    test: protect platform commerce forms

--- organizations/test_internal_content_forms.py
+++ organizations/test_internal_content_forms.py
@@ -1,6 +1,8 @@
 from django.test import SimpleTestCase
 
 from organizations.forms.content import OrganizationCourseForm, OrganizationExamTrackForm
+from courses.forms import CourseForm
+from quiz.forms import ExamTrackForm
 
 
 class OrganizationInternalContentFormTests(SimpleTestCase):
@@ -18,3 +20,19 @@ class OrganizationInternalContentFormTests(SimpleTestCase):
             "currency",
         }
         self.assertTrue(commercial_fields.isdisjoint(OrganizationExamTrackForm.Meta.fields))
+
+    def test_platform_course_form_retains_commercial_fields(self):
+        self.assertIn("subscription_plans", CourseForm.Meta.fields)
+        self.assertIn("is_public", CourseForm.Meta.fields)
+        self.assertIn("is_published", CourseForm.Meta.fields)
+
+    def test_platform_track_form_retains_commercial_fields(self):
+        for field in (
+            "subscription_plans",
+            "pricing_type",
+            "monthly_price",
+            "lifetime_price",
+            "trial_days",
+            "currency",
+        ):
+            self.assertIn(field, ExamTrackForm.Meta.fields)
