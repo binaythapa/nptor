30a8967ce5303bb6761bee9e91482dfd164bfa47
Binay Thapa <37831189+binaythapa@users.noreply.github.com>
2026-09-13 20:46:20 +0545

    fix: scope Track progression exams by organization

--- quiz/views/admin_track_product_views.py
+++ quiz/views/admin_track_product_views.py
@@ -1,4 +1,5 @@
 from django.contrib.admin.views.decorators import staff_member_required
+from django.db import transaction
 from django.shortcuts import get_object_or_404, redirect, render
 
 from quiz.models import ExamTrack
@@ -6,23 +7,30 @@ from quiz.track_forms import TrackExamFormSet
 from quiz.views.admin_subscription_views import TrackForm
 
 
-@staff_member_required
-def admin_track_create(request):
-    organization = None
-    form = TrackForm(request.POST or None, organization=organization)
-    formset = TrackExamFormSet(
+def _track_formset(request, instance, organization):
+    return TrackExamFormSet(
         request.POST or None,
-        instance=ExamTrack(),
+        instance=instance,
         organization=organization,
         prefix="track_exams",
     )
 
+
+@staff_member_required
+def admin_track_create(request):
+    form = TrackForm(request.POST or None)
+    organization = None
+    if request.method == "POST" and form.is_valid():
+        organization = form.cleaned_data.get("organization")
+    formset = _track_formset(request, ExamTrack(), organization)
+
     if request.method == "POST" and form.is_valid() and formset.is_valid():
-        track = form.save(commit=False)
-        track.save()
-        form.save_m2m()
-        formset.instance = track
-        formset.save()
+        with transaction.atomic():
+            track = form.save(commit=False)
+            track.save()
+            form.save_m2m()
+            formset.instance = track
+            formset.save()
         return redirect("quiz:admin_track_list")
 
     return render(
@@ -35,18 +43,16 @@ def admin_track_create(request):
 @staff_member_required
 def admin_track_update(request, pk):
     track = get_object_or_404(ExamTrack, pk=pk)
+    form = TrackForm(request.POST or None, instance=track, organization=track.organization)
     organization = track.organization
-    form = TrackForm(request.POST or None, instance=track, organization=organization)
-    formset = TrackExamFormSet(
-        request.POST or None,
-        instance=track,
-        organization=organization,
-        prefix="track_exams",
-    )
+    if request.method == "POST" and form.is_valid():
+        organization = form.cleaned_data.get("organization")
+    formset = _track_formset(request, track, organization)
 
     if request.method == "POST" and form.is_valid() and formset.is_valid():
-        form.save(commit=True)
-        formset.save()
+        with transaction.atomic():
+            form.save(commit=True)
+            formset.save()
         return redirect("quiz:admin_track_list")
 
     return render(
