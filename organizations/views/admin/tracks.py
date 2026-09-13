6fbbc1e3f09555046c7da8818f6cafea34ed2c6c
Binay Thapa <37831189+binaythapa@users.noreply.github.com>
2026-09-13 19:31:42 +0545

    fix: scope organization track forms

--- organizations/views/admin/tracks.py
+++ organizations/views/admin/tracks.py
@@ -4,12 +4,22 @@ from django.views.decorators.http import require_POST
 from organizations.permissions import org_admin_required
 from quiz.models import ExamTrack
 from quiz.forms import ExamTrackForm
+from quiz.track_forms import TrackExamFormSet
+
+
+def _track_formset(request, track, organization):
+    return TrackExamFormSet(
+        request.POST or None,
+        instance=track,
+        organization=organization,
+        prefix="track_exams",
+    )
 
 
 @org_admin_required
 def org_track_list(request, slug):
     org = request.organization
-    tracks = ExamTrack.objects.filter(organization=org).order_by("-created_at")
+    tracks = ExamTrack.objects.filter(organization=org).prefetch_related("track_exams__exam").order_by("-created_at")
     return render(
         request,
         "organizations/admin/tracks/list.html",
@@ -21,14 +31,14 @@ def org_track_list(request, slug):
 def org_track_create(request, slug):
     org = request.organization
     if request.method == "POST":
-        form = ExamTrackForm(request.POST)
+        form = ExamTrackForm(request.POST, organization=org)
         if form.is_valid():
             track = form.save(commit=False)
             track.organization = org
             track.save()
             return redirect("organizations_admin:org_track_list", slug=slug)
     else:
-        form = ExamTrackForm()
+        form = ExamTrackForm(organization=org)
     return render(
         request,
         "organizations/admin/tracks/create.html",
@@ -41,14 +51,14 @@ def org_track_edit(request, slug, pk):
     org = request.organization
     track = get_object_or_404(ExamTrack, pk=pk, organization=org)
     if request.method == "POST":
-        form = ExamTrackForm(request.POST, instance=track)
+        form = ExamTrackForm(request.POST, instance=track, organization=org)
         if form.is_valid():
             updated = form.save(commit=False)
             updated.organization = org
             updated.save()
             return redirect("organizations_admin:org_track_list", slug=slug)
     else:
-        form = ExamTrackForm(instance=track)
+        form = ExamTrackForm(instance=track, organization=org)
     return render(
         request,
         "organizations/admin/tracks/edit.html",
