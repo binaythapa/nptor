5a908b5fa7a7c5655e6db4120b88eb740820085c
Binay Thapa <37831189+binaythapa@users.noreply.github.com>
2026-09-14 02:21:07 +0545

    feat: add student contact field to profile form

--- organizations/forms/student.py
+++ organizations/forms/student.py
@@ -4,13 +4,29 @@ from organizations.models import OrganizationStudent
 
 
 class OrganizationStudentProfileForm(forms.ModelForm):
-    first_name = forms.CharField(max_length=150, required=False)
-    last_name = forms.CharField(max_length=150, required=False)
-    email = forms.EmailField(disabled=True, required=False)
+    first_name = forms.CharField(max_length=150, required=False, label="First name")
+    last_name = forms.CharField(max_length=150, required=False, label="Last name")
+    email = forms.EmailField(disabled=True, required=False, label="Email")
+    contact_phone = forms.CharField(max_length=50, required=False, label="Contact number")
 
     class Meta:
         model = OrganizationStudent
-        fields = ["first_name", "last_name", "email", "date_of_birth", "guardian_name", "guardian_phone", "address"]
+        fields = [
+            "first_name",
+            "last_name",
+            "email",
+            "contact_phone",
+            "date_of_birth",
+            "guardian_name",
+            "guardian_phone",
+            "address",
+        ]
+        labels = {
+            "date_of_birth": "Date of birth",
+            "guardian_name": "Guardian name",
+            "guardian_phone": "Guardian contact number",
+            "address": "Address",
+        }
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
@@ -18,6 +34,9 @@ class OrganizationStudentProfileForm(forms.ModelForm):
             self.fields["first_name"].initial = self.instance.user.first_name
             self.fields["last_name"].initial = self.instance.user.last_name
             self.fields["email"].initial = self.instance.user.email
+            profile = getattr(self.instance.user, "profile", None)
+            if profile:
+                self.fields["contact_phone"].initial = profile.phone
 
     def save(self, commit=True):
         instance = super().save(commit=False)
