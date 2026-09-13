d5d6b5d7304a571bb16bdac07093225110e0c501
Binay Thapa <37831189+binaythapa@users.noreply.github.com>
2026-09-14 02:40:39 +0545

    fix: restore applied quiz migration merge

---
+++ quiz/migrations/0015_merge_20260914_0150.py
@@ -0,0 +1,10 @@
+from django.db import migrations
+
+
+class Migration(migrations.Migration):
+    dependencies = [
+        ("quiz", "0014_merge_20260913_2154"),
+        ("quiz", "0014_remove_exam_primary_category"),
+    ]
+
+    operations = []
