eccbde9bb9c3623f19a36ef7f649c20ba9f13af1
Binay Thapa <37831189+binaythapa@users.noreply.github.com>
2026-09-15 19:02:31 +0545

    fix: remove nonexistent subscriptions migration dependency

--- subscriptions/migrations/0007_merge_20260914_0150.py
+++ subscriptions/migrations/0007_merge_20260914_0150.py
@@ -3,7 +3,6 @@ from django.db import migrations
 
 class Migration(migrations.Migration):
     dependencies = [
-        ("subscriptions", "0006_rename_sub_plan_scope_active_idx_subscriptio_scope_240a07_idx_and_more"),
         ("subscriptions", "0006_subscriptionplan_billing_interval"),
     ]
 
