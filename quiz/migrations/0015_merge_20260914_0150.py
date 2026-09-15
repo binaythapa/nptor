2330c2b92dd21945b1347df1676fc644c23eaae6
Binay Thapa <37831189+binaythapa@users.noreply.github.com>
2026-09-15 19:17:17 +0545

    fix: merge quiz migration branches

--- quiz/migrations/0015_merge_20260914_0150.py
+++ quiz/migrations/0015_merge_20260914_0150.py
@@ -3,6 +3,7 @@ from django.db import migrations
 
 class Migration(migrations.Migration):
     dependencies = [
+        ("quiz", "0013_remove_examtrack_courses_and_more"),
         ("quiz", "0014_remove_exam_primary_category"),
     ]
 
