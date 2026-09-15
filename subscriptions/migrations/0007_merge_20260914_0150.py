307e4f5a7cb04b123c81001a4a1735778bed5f8e
Binay Thapa <37831189+binaythapa@users.noreply.github.com>
2026-09-15 21:33:44 +0545

    chore: reset Django migrations

--- subscriptions/migrations/0007_merge_20260914_0150.py
+++
@@ -1,9 +0,0 @@
-from django.db import migrations
-
-
-class Migration(migrations.Migration):
-    dependencies = [
-        ("subscriptions", "0006_subscriptionplan_billing_interval"),
-    ]
-
-    operations = []
